2133de60f2b9cb2b9ded73d47857bc568608133c
João Ramos <jvitorsb98@outlook.com>
2024-06-08 15:02:58 -0300

    Arquivo faltantes da seção 2

---
+++ JavaScript-Logica/Aula01_Comparacao/script.js
@@ -0,0 +1,13 @@
+/*
+Operadores de comparação 
+> maior que 
+< menor que 
+>= maior igual a que
+<= menor igual a que
+== igualdade (valor)
+=== igualdade(valor,tipo)
+!= desigualdade (valor)
+!== desigualdade(valor,tipo)
+
+
+*/
