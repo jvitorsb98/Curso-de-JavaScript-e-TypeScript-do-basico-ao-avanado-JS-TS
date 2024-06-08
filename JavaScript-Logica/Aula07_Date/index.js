9e3a325e6d04f80c228b4d2e167cda4a6ab1eff8
João Ramos <jvitorsb98@outlook.com>
2024-06-08 20:02:29 -0300

    Atividade Date

---
+++ JavaScript-Logica/Aula07_Date/index.js
@@ -0,0 +1,7 @@
+const oneDay = 60 * 60 * 24 * 1000;
+const data = new Date(oneDay)
+const data2 = new Date(2024,3,20,15,14,6,50)
+const data3 = new Date("2019-4-20 20:20:58");
+console.log(data.toString());
+console.log(data2.toString());
+console.log(data3.toString());
